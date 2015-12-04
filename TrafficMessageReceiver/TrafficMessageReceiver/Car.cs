da6030bbb45e89033bcdfa61d0940a1a0b433019
Kevin Dullens <kevindullens3@hotmail.com>
2015-12-04 11:42:04 +0100

    Added TrafficMessageService and TrafficMessageReceiver

---
+++ TrafficMessageReceiver/TrafficMessageReceiver/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TrafficMessageReceiver2
+{
+    class Car
+    {
+        public string Licenseplate { get; private set; }
+        public string Fueltype { get; private set; }
+        public string CarInfo { get; private set; }
+
+        public Car (string licenseplate, string fueltype, string carinfo)
+        {
+            Licenseplate = licenseplate;
+            Fueltype = fueltype;
+            CarInfo = carinfo;
+        }
+
+        public override string ToString()
+        {
+            return Licenseplate + "," + Fueltype + "," + CarInfo;
+        }
+    }
+}
---
+++ TrafficMessageService/Server/CTrafficMessage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MessageService;
+using System.ServiceModel;
+
+namespace Server
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class CTrafficMessage : ITrafficMessage
+    {
+        private List<string> trafficMessages;
+
+        CTrafficMessage()
+        {
+            trafficMessages = new List<string>();
+        }
+
+        public string GetServerName()
+        {
+            return "Smart Tankstation Server";
+
+        }
+
+        public void SendMessage(string message, int adress)
+        {
+                trafficMessages.Add(adress.ToString() + message);
+        }
+
+       public string RetrieveMessage(int adress)
+        {
+                int counter = 0;
+
+                foreach (string item in trafficMessages)
+	                {
+		                if(item.Substring(0,1) == adress.ToString())
+                        {
+                              string returnstring = item.Substring(1,item.Length-1);
+                              trafficMessages.RemoveAt(counter);
+                              counter++;
+                              return returnstring;              
+                        }
+	                }
+
+                return "";                     
+         }   
+    }          
+ }
